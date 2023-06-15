9ad4c595a4367102a2829ad4ebb3510ce9827c0b
youssef tadlaoui <usftadlaoui@gmail.com>
2023-06-15 23:57:10 +0100

    cp stats

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 
 export default function App(){
+return(
+<div className="app"> 
+<Logo/>
+<Form/>
+<PackingList/>
+<Stats/>
 
+</div>
+)
 }
 
 
@@ -13,5 +21,15 @@ function Form(){
     <div className="add-form">
       <h3>what do oyu nedd for ur trip?</h3>
     </div>
+  )
+}
+function PackingList(){
+  return <div className="list">List</div>
+}
+function Stats(){
+  return(
+    <footer className="stats">
+      <em>You have x items on your list, and you already packes X (X%)</em>
+    </footer>
   )
 }
