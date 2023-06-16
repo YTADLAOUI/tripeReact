5fa50473f5310f87d9f04064c7bd6cd5034fbb49
youssef tadlaoui <usftadlaoui@gmail.com>
2023-06-16 01:18:12 +0100

    style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 
+import {React,useState} from "react";
+
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
 export default function App(){
 return(
 <div className="app"> 
@@ -24,7 +32,22 @@ function Form(){
   )
 }
 function PackingList(){
-  return <div className="list">List</div>
+  return (
+  <div className="list">
+      <ul >{initialItems.map((item)=>(
+        <Item item={item}/>
+      ))}
+    </ul>
+  </div>
+  )
+}
+function Item({item}){
+return (
+  <il>
+      <span style={item.packed?{textDecoration:"line-through"}:{}}>{item.quantity} {item.description}
+      </span>
+      <button style={{color:'red', border:'none',backgroundColor:'transparent',fontSize:'3rem'}}>&times;</button>
+  </il>)
 }
 function Stats(){
   return(
