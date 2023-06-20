51c2a7fa8c16466aac1e3e9182f4a58ad11c48df
youssef tadlaoui <usftadlaoui@gmail.com>
2023-06-20 16:50:44 +0100

    sortbydesc

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,13 +1,25 @@
+import { useState } from "react"
 import Item from "./item"
 export default function PackingList({initialItems,onToggleItem,onDeleteItem,onClearList}){
- 
+ const [sortBy,setSortBy]=useState('input')
+let sortedItems;
+
+ if(sortBy=='input') sortedItems=initialItems;
+ if(sortBy=="description") sortedItems=initialItems.slice().sort((a, b) => a.description.localeCompare(b.description));
   return (
   <div className="list">
-      <ul >{initialItems.map((item)=>(
+      <ul >{sortedItems.map((item)=>(
         <Item item={item} key={item.id} onToggleItem={onToggleItem} onDeleteItem={onDeleteItem}/>
       ))}
     </ul>
-    <button onClick={onClearList}>Clear list</button>
+    <div className="actions">
+        <select value={sortBy} onChange={(e)=>setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClearList}>Clear list</button>
+      </div>
   </div>
   )
 }
